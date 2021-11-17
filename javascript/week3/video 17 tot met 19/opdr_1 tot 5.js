e754d95f2b0333fbf5829449f1ed0361485293b0
DanielKool <daniel@familiekool.nl>
2021-11-17 10:50:55 +0100

    vaardigheids opdracht video 17 t/m 19

---
+++ javascript/week3/video 17 tot met 19/opdr_1 tot 5.js
@@ -0,0 +1,19 @@
+firstName = "Daniel";
+lastName = "Kool";
+age = 19;
+
+console.log( firstName, lastName , age);
+
+const zin = `Mijn naam is ${firstName}`;
+console.log(zin);
+
+const naam = `Mijn volledige naam is ${ firstName} ${lastName}`;
+console.log(naam);
+
+let birthYear = 2002;
+let year = 2056;
+const leefttijd = `In ${year} ben ${year-birthYear}`;
+console.log(leefttijd);
+
+const familieNaam = `Mijn naam is ${firstName} en ik ben ${age} jaar oud. Daarnaast is mijn familienaam ${lastName}`;
+console.log(familieNaam);
---
+++ javascript/week3/video 17 tot met 19/opdr_6 tot 9.js
@@ -0,0 +1,35 @@
+const weather = "regen";
+
+if (weather === "regen"){
+    console.log("ik neem mijn paraplu mee naar buiten");
+} else {
+    console.log("ik zet mijn zonnebril op 🕶");
+}
+
+const grass = "yellow";
+
+if (grass === "yellow"){
+    console.log("ik geef het gras water");
+} else {
+    console.log("ik doe niks");
+}
+
+const season = "winter";
+
+if (season === "winter"){
+    console.log("ik zet mijn verwarming aan");
+} else {
+    console.log("dans ik op de tafel");
+}
+
+const weer = "kleihard-regen";
+
+if (weer === "regen"){
+       console.log("ik neem mijn paraplu mee naar buiten");
+} 
+else if ( weer === "kleihard-regen") {
+    console.log(" ik trek een regenpak aan ");
+}
+else {
+    console.log("ik zet mijn zonnebril op 🕶");
+}
