44fc50060f401adfc231cfa54635d3814be7b897
DanielKool <daniel@familiekool.nl>
2021-11-17 13:04:39 +0100

    opdrachten 1 t/m 5

---
+++ javascript/week3/vaardigheids opdrachten/video 20 tot met 25/opdr_1t5.js
@@ -0,0 +1,14 @@
+const x = 1;
+const y = "1";
+const z = 2;
+
+console.log(x + y - z)
+
+console.log(z - y - x)
+
+console.log(z + y - x)
+
+console.log(String(x) + String(z) + y)
+
+const w ="2"
+console.log( String(x) + (w * 2) + String(z * 2))
